0939c3261b259e89625846f5c6c066c5f1db6d1a
Anubhab Sen <anubhabsen@gmail.com>
2018-04-11 17:46:49 +0530

    sequential data loader stub

---
+++ code/datasets/folders.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.misc import imread
+import random
+from path import Path
+import torch.utils.data
+
+class SequenceFolder(torch.utils.data.Dataset):
+	def __init__(self):
+		pass
