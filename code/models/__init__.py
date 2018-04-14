a186579656669841e59fe90c94c01bbc0b3d2257
Aditya Bharti <adibhar97@gmail.com>
2018-04-14 06:47:13 +0530

    Fixed broken checkpointing code.

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -20,25 +20,29 @@ def setup(disp_model, pose_model, opt):
     return disp_model, pose_model, optimizer
 
 # def save_checkpoint(opt, model, optimizer, best_acc, epoch):
-def save_checkpoint(opt, model, optimizer, epoch):
+def save_checkpoint(opt, disp_model, pose_model, optimizer, epoch):
     state = {
         'epoch': epoch + 1,
         'arch': opt.model_def,
-        'state_dict': model.state_dict(),
+        # 'state_dict': model.state_dict(),
+        'disp_state_dict': disp_model.state_dict(),
+        'pose_state_dict': pose_model_state_dict(),
         # 'best_prec1': best_acc,
         'optimizer' : optimizer.state_dict(),
     }
     filename = "savedmodels/" + opt.name + ".pth.tar"
     torch.save(state, filename)
 
-def resumer(opt, model, optimizer):
+def resumer(opt, disp_model, pose_model, optimizer):
     if os.path.isfile(opt.resume):
         print("=> loading checkpoint '{}'".format(opt.resume))
         checkpoint = torch.load(opt.resume)
         opt.start_epoch = checkpoint['epoch']
         # best_prec1 = checkpoint['best_prec1']
-        model.load_state_dict(checkpoint['state_dict'])
+        # model.load_state_dict(checkpoint['state_dict'])
+        disp_model.load_state_dict(checkpoint['disp_state_dict'])
+        pose_model.load_state_dict(checkpoint['pose_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         print("=> loaded checkpoint '{}' (epoch {})".format(opt.resume, checkpoint['epoch']))
         # return model, optimizer, opt, best_prec1
-        return model, optimizer, opt
+        return disp_model, pose_model, optimizer, opt
